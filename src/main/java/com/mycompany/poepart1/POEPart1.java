a438efd274f8aa19cf1499e556a26230a9034fef
RC_Student_lab <RC_Student_lab@10.0.0.18>
2024-09-26 17:54:56 +0000

    Created a Registration Class and Login Class, Scanner objects and declared variables.

--- src/main/java/com/mycompany/poepart1/POEPart1.java
+++ src/main/java/com/mycompany/poepart1/POEPart1.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.poepart1;
+import java.util.*;
 
 /**
  *
@@ -11,6 +12,21 @@ package com.mycompany.poepart1;
 public class POEPart1 {
 
     public static void main(String[] args) {
+        //Login Instance
+        Registration check = new Registration();
+        Login check_all = new Login();
+        
+        //Object for a Scanner
+        Scanner input = new Scanner(System.in);
+        
+        //Declarations
+        String username;
+        String password;
+        String name;
+        String surname;
+        
+        
+        
         
     }
 }
