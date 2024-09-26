7d70760d46d0d234f68eec74259b4f1d54ad20a3
RC_Student_lab <RC_Student_lab@10.0.0.18>
2024-09-26 19:25:38 +0000

    Check the password format in the registration class

--- src/main/java/com/mycompany/poepart1/Registration.java
+++ src/main/java/com/mycompany/poepart1/Registration.java
@@ -13,7 +13,7 @@ public class Registration {
 
     public boolean checkUsername(String username) {
         //Temporary variable for checking
-        boolean Found;
+        boolean Found = false;
         //Check username
         if (username.contains("_") && username.length() <5){
             Found = true;
@@ -27,7 +27,26 @@ public class Registration {
         }
         return Found;
     }
-    
+    public boolean checkPassword(String password){
+        
+        //Check password
+        Pattern check_num = Pattern.compile("[0123456789]");
+        Pattern check_specials = Pattern.compile("[!@#$%^&*+-_:.<,>~']");
+        Pattern check_Uppercase = Pattern.compile("[QWERTYUIOPLKJHGFDSAZXCVBNM]");
+        
+        boolean Found = false;
+        //Check all
+        if(check_num.matcher(password).find() && check_specials.matcher(password).find() && check_Uppercase.matcher(password).find()){
+            Found =true;
+            //Message
+            System.out.println("Password successfully captured");
+        }else{
+            System.out.println("Password is not correctly formatted, please "
+                    + "ensure that the password containsat least 8 characters,"
+                    + "a capital letter, a number and a special character.");
+        }
+        return Found;
+    }   
 
     
 }
