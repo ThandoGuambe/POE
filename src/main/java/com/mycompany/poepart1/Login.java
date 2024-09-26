86a00e0494024d59e5b64edbb4e6d3ad315a8688
RC_Student_lab <RC_Student_lab@10.0.0.18>
2024-09-26 19:24:24 +0000

    Check the username format in the registration class

--- src/main/java/com/mycompany/poepart1/Login.java
+++ src/main/java/com/mycompany/poepart1/Login.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.poepart1;
+import java.util.regex.Pattern;
 
 /**
  *
--- src/main/java/com/mycompany/poepart1/Registration.java
+++ src/main/java/com/mycompany/poepart1/Registration.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.poepart1;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -10,7 +11,23 @@ package com.mycompany.poepart1;
  */
 public class Registration {
 
-    public Registration() {
+    public boolean checkUsername(String username) {
+        //Temporary variable for checking
+        boolean Found;
+        //Check username
+        if (username.contains("_") && username.length() <5){
+            Found = true;
+            //Message
+            System.out.println("Username is successfully captured");
+        }else{
+            Found= false;
+            System.out.println("Username is not formatted, please ensure that "
+                    + "your usernamecontains an underscore and is no more "
+                    + "than 5 characters in length.");
+        }
+        return Found;
     }
+    
+
     
 }
